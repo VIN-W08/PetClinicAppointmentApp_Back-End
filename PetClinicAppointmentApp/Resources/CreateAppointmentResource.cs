e1f3496ccd966f9bf6822ecf13a34c3d5474d077
VIN-W08 <vcode0808@gmail.com>
2022-08-31 22:33:55 +0700

    changes to class definition, and delete a pet clinic image file

--- PetClinicAppointmentApp/Resources/CreateAppointmentResource.cs
+++ PetClinicAppointmentApp/Resources/CreateAppointmentResource.cs
@@ -1,24 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
 
 namespace PetClinicAppointmentApp.Resources
 {
     public class CreateAppointmentResource
     {
-        [Required]
         public int Customer_id { get; set; }
-        [Required]
         public int Pet_clinic_id { get; set; }
-        [Required]
         public int Service_id { get; set; }
-        [Required]
         public int Schedule_service_id { get; set; }
-        [Required]
         public string Service_name { get; set; }
-        [Required]
         public decimal Service_price { get; set; }
-        [Required]
         public DateTime Start_schedule { get; set; }
-        [Required]
         public DateTime End_schedule { get; set; }
     }
 }
--- PetClinicAppointmentApp/Resources/CreateServiceScheduleResource.cs
+++ PetClinicAppointmentApp/Resources/CreateServiceScheduleResource.cs
@@ -1,16 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
 
 namespace PetClinicAppointmentApp.Resources
 {
     public class CreateServiceScheduleResource
     {
-        [Required]
         public int Service_id { get; set; }
-        [Required]
         public DateTime Start_schedule { get; set; }
-        [Required]
         public DateTime End_schedule { get; set; }
-        [Required]
         public int Quota { get; set; } = 1;
         public int Repeat_schedule_Week_count { get; set; } = 0;
     }
